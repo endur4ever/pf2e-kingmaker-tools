1964790c0224e96f1db4f6632354814ecc8bbb28
endur4ever <gregoryziegler@gmail.com>
2026-07-08 17:26:26 -0700

    chore: clean up uncommitted changes

--- src/jsMain/kotlin/at/posselt/pfrpg2e/kingdom/sheet/KingdomSheet.kt
+++ src/jsMain/kotlin/at/posselt/pfrpg2e/kingdom/sheet/KingdomSheet.kt
@@ -2906,7 +2906,6 @@ class KingdomSheet(
                 "fame" to "kingdom.analytics.fame",
                 "xpAwarded" to "kingdom.analytics.xpAwarded",
                 "warPressure" to "kingdom.analytics.warPressure",
-                "pressurePerTurn" to "kingdom.analytics.pressurePerTurn",
                 "level" to "kingdom.analytics.level",
                 "size" to "kingdom.analytics.size",
                 "ruinCorruption" to "kingdom.analytics.ruinCorruption",
